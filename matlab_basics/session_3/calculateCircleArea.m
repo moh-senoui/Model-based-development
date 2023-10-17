be474cdb5278e32eb657476a72492b37c65259cf
Mohamed bakr <131889034+moh-senoui@users.noreply.github.com>
2023-10-17 13:40:49 +0300

    Add files via upload

---
+++ matlab_basics/session_3/calculateCircleArea.m
@@ -0,0 +1,4 @@
+%Task 2: Function Inputs and Outputs
+function area = calculateCircleArea(radius)
+    area = pi * radius ^2 ;
+end
---
+++ matlab_basics/session_3/calculateCircleArea_res.m
@@ -0,0 +1,3 @@
+r = 2;
+res = calculateCircleArea(r);
+fprintf('area of circle %.2f',res);
