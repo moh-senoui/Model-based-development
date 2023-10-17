ebb99ffa7df637ecd9d3f82ec82f0b8a4f0ec49d
Mohamed bakr <131889034+moh-senoui@users.noreply.github.com>
2023-10-17 14:19:57 +0300

    Add files via upload

---
+++ matlab_basics/session_3/computeStatistics.m
@@ -0,0 +1,5 @@
+%Task 3: Function with Multiple Outputs
+function [mean_val , std_dev] = computeStatistics(numarr)
+mean_val = mean(numarr);
+std_dev = std(numarr);
+end
---
+++ matlab_basics/session_3/computeStatistics_res.m
@@ -0,0 +1,3 @@
+numarr = [1,2,3,4,8,7,9];
+[mean_res, stddiv_res] = computeStatistics(numarr);
+fprintf(' mean = %.2f \n  std_div = %.2f', mean_res,stddiv_res);
