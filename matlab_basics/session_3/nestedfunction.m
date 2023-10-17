f824420976e747fdc9a52b6f399a09a644bbff40
Mohamed bakr <131889034+moh-senoui@users.noreply.github.com>
2023-10-17 14:55:00 +0300

    Add files via upload

---
+++ matlab_basics/session_3/nestedfunction.m
@@ -0,0 +1,15 @@
+%Task 4: Nested Functions
+
+function frist =  outerFunction()
+      call =  innerFunction(9,2);
+      fprintf('sum = %d ',call );
+end
+
+function secound = innerFunction(a,b)
+       secound = a + b ;
+
+end
+
+
+
+
